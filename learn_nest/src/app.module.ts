7f114ecaf9160faa2fdc7238bbfdf03235679da6
Seunghan Park <seunpark@student.42seoul.kr>
2023-03-25 19:57:11 +0900

    안쓰는 파일 정리

--- learn_nest/src/app.module.ts
+++ learn_nest/src/app.module.ts
@@ -2,18 +2,16 @@ import { Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
 import { MiddlewareConsumer } from '@nestjs/common';
-import { CatsController } from './controller/cat.controller';
-import { CatsService } from './service/cat.service';
-import { CatsModule } from './module/cats.module';
 import { logger, LoggerMiddleware } from './middleware/logger.middleware';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './module/auth.module';
+import { AuthController } from './controller/auth.controller';
 
 @Module({
-  imports: [AuthModule, CatsModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [AuthModule, TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
   // providers: [AppService, {provide: APP_FILTER , useClass: HttpExceptionFilter}],
@@ -25,10 +23,7 @@ export class AppModule implements NestModule {
       .apply(LoggerMiddleware)
       // .exclude({ path: 'cats', method: RequestMethod.GET }, { path: 'cats', method: RequestMethod.POST }, 'cats/(.*)')
       // .forRoutes('cats/test');
-      .forRoutes(CatsController);
-    // .forRoutes('cats');
-    // .forRoutes({path: "cats", method: RequestMethod.GET});
-
+      .forRoutes(AuthController, AppController);
     //함수형 미들웨어
     // consumer.apply(logger).forRoutes(CatsController);
   }
--- learn_nest/src/controller/app.controller.ts
+++ learn_nest/src/controller/app.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
 import { AppService } from '../service/app.service';
 import { Res, HttpCode, Redirect } from '@nestjs/common';
 
-import { CreateCatDto } from '../dto/create-cat.dto';
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -11,51 +9,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('/echo/:str')
-  echoParam(@Param('str') id: string, @Res() res): string {
-    let response = id + '  test';
-    return res.status(200).send({ response });
-  }
-
-  @Get('/query/')
-  echoQuery(
-    @Query('id') id: string,
-    @Query('pw') pw: string,
-    @Res() res,
-  ): string {
-    if (id == null || pw == null) {
-      let response = 'failed';
-      return res.status(200).send({ response });
-    }
-    if (id == pw) {
-      let response = `id : ${id}, pw: ${pw}`;
-      return res.status(200).send({ response });
-    }
-    let response = 'failed';
-    return res.status(200).send({ response });
-  }
-
-  @Get('/status')
-  @HttpCode(201)
-  status(@Res() res): string {
-    return res.status(201).send('123');
-  }
-
-  @Get('/redirection')
-  @Redirect('https://naver.com', 301)
-  redirection(): string {
-    return 'check code';
-  }
-
-  @Post()
-  post(@Body() dto: CreateCatDto, @Res() res): string {
-    if (dto.name === dto.breed) {
-      let response = `name : ${dto.name}, breed: ${dto.breed}`;
-      return res.status(200).send({ response });
-    }
-    let response = `not same`;
-    return res.status(200).send({ response });
   }
 }
